080ee0bee36fff3b577cd28aafd102077200803e
Muhammad Abdullah <muhammad.abdullah.0913@gmail.com>
2025-01-10 22:48:34 -0500

    ready for build and deploy

--- main.tf
+++ main.tf
@@ -6,7 +6,7 @@ resource "aws_instance" "app" {
     ami = "ami-07dae6a161c5c4239"
     instance_type = "t2.micro"
     tags = {
-        Name = "angular-heroes-app"
+        Name = "heroes-angular-app"
     }
 }
 
